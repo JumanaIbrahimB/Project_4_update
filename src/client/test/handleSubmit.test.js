1d507d263171a042b5ed18ed1714aad22e9252da
JumanaIbrahimB <97875817+JumanaIbrahimB@users.noreply.github.com>
2022-03-17 12:44:16 +0300

    Update handleSubmit.test.js

--- src/client/test/handleSubmit.test.js
+++ src/client/test/handleSubmit.test.js
@@ -1,18 +1,17 @@
 import { handleSubmit } from '../js/formHandler'
 
-
-describe('Test, the function "handleSubmit()" should exist' , () => {
+// test if the function is exist
     test('It should return true', async () => {
         expect(handleSubmit).toBeDefined();
     });
-});
-describe('Test, the function "handleSubmit()" should be a function' , () => {
-    test('It should be a function', async () => {
-        expect(typeof handleSubmit).toBe("function");
-    });
-});
-describe('Test, the function "handleSubmit()" should not be null' , () => {
+
+// test the funtion if it is null
     test('It should return true', async () => {
         expect(handleSubmit).not.toBeNull();
     });
+    
+// test the function if it is function
+test('It should be a function', async () => {
+    expect(typeof handleSubmit).toBe("function");
+
 });
